a52e40401a6541c95615836ea9c36f9d3222618c
dajo-25 <DjBosch25@gmail.com>
2021-11-16 01:11:27 +0100

    The executable of hipnosis has been created

---
+++ projectes/hipnosis/application.windows64/source/hipnosis.java
@@ -0,0 +1,134 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class hipnosis extends PApplet {
+
+
+public void setup(){
+  
+  frameRate(120);
+}
+
+float wRect = 300;
+float hRect = 40;
+
+int r;
+int radiRect;
+float parametre = 1;
+
+ChangingColor rRect = new ChangingColor();
+ChangingColor gRect = new ChangingColor();
+ChangingColor bRect = new ChangingColor();
+
+public void draw(){
+  
+  if(radiRect > 1720){
+    parametre = -abs(parametre);
+  }
+  if(radiRect < 1){
+    parametre = abs(parametre);  
+  }
+  
+  radiRect += parametre * rRect.updatedColor()/10;
+  
+  hRect = radiRect;
+  wRect = radiRect;
+
+  r += 1;
+  translate((width)/2, (height)/2);
+  noFill();
+  stroke(rRect.updatedColor(), gRect.updatedColor(), bRect.updatedColor());
+  strokeWeight(60);
+  rotate(r);
+  rect(-wRect/2, -hRect/2, wRect, hRect);
+}
+
+public class ChangingColor{
+    
+  float valorColor;
+  float velocitatColor;
+  float parametreAcceleracio;
+  float minim;
+  float maxim;
+  
+    public ChangingColor(){
+      
+      minim = 0;
+      maxim = 255;
+      
+      valorColor = random(minim, maxim);
+      velocitatColor = 0;
+      parametreAcceleracio = 0.5f;
+      
+    }
+    
+    public ChangingColor(float acceleracio){
+      
+      minim = 0;
+      maxim = 255;
+      
+      valorColor = random(minim, maxim);
+      velocitatColor = 0;
+      parametreAcceleracio = acceleracio;
+    }
+    
+    public ChangingColor(float min, float max){
+      
+      minim = min;
+      maxim = max;
+      
+      valorColor = random(minim, maxim);
+      velocitatColor = 0;
+      parametreAcceleracio = 1;
+    }
+    
+    public ChangingColor(float acceleracio, float min, float max){
+      
+      minim = min;
+      maxim = max;
+      
+      valorColor = random(minim, maxim);
+      velocitatColor = 0;
+      parametreAcceleracio = acceleracio;
+    }
+    
+    public float updatedColor(){
+      valorColor += velocitatColor;
+      velocitatColor += random(-parametreAcceleracio, parametreAcceleracio);
+      
+      corregirVelocitat();
+      
+      return valorColor;
+    }
+    
+    public void corregirVelocitat(){
+      if(valorColor <= minim){
+        velocitatColor = 2;
+      }
+      if(valorColor >= maxim){
+        velocitatColor = -2;
+      }
+    }
+    
+}
+  public void settings() {  fullScreen(); }
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "--present", "--window-color=#666666", "--stop-color=#cccccc", "hipnosis" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
